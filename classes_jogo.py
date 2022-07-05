ffd795ed234fac0ae1e645610813f85748c9eb12
Rafael <rafaeldutraribas@gmail.com>
2022-07-05 11:11:23 -0300

    OBJETOS_JOGO RENOMEADO objetos_jogo.py renomeado para classes_jogo.py

---
+++ classes_jogo.py
@@ -0,0 +1,60 @@
+class Juiz:
+    def __init__(self):
+        self.placar_jogador = [False, False, False]
+        self.placar_bot = [False, False, False]
+        self.elementos_cartas = ['agua', 'fogo', 'gelo']
+
+    def qual_carta_ganha_a_rodada_retorna_none_caso_empate(self, carta_jogador, carta_bot):
+        jogador_ganhou = 0
+        bot_ganhou = 1
+
+        resultado = self.qual_elemento_ganha_retorna_none_caso_empate(carta_jogador.elemento, carta_bot.elemento)
+
+        mesmo_elemento = resultado is None
+        elemento_do_jogador_ganha = resultado == 0
+
+        if mesmo_elemento:
+            if carta_jogador.valor == carta_bot:
+                return None
+
+            if carta_jogador.valor > carta_bot.valor:
+                return jogador_ganhou
+
+        if elemento_do_jogador_ganha:
+            return jogador_ganhou
+        else:
+            return bot_ganhou
+
+    def qual_elemento_ganha_retorna_none_caso_empate(self, elemento_0, elemento_1):
+        index_elemento_0 = self.elementos_cartas.index(elemento_0)
+        index_elemento_1 = self.elementos_cartas.index(elemento_1)
+
+        if index_elemento_0 == index_elemento_1:
+            return None
+
+        if index_elemento_1 == (index_elemento_0+1)//3 or index_elemento_0+1 == index_elemento_1-1:
+            return 1
+        else:
+            return 0
+
+
+    def contabiliza_no_placar_do_ganhador_da_rodada(self, ganhador, elemento):
+        index_para_ser_contabilizado = self.elementos_cartas.index(elemento)
+        jogador_ganhou = ganhador == 0
+        bot_ganhou = ganhador == 1
+
+        if jogador_ganhou:
+            self.placar_jogador[index_para_ser_contabilizado] = True
+
+        elif bot_ganhou:
+            self.placar_bot[index_para_ser_contabilizado] = True
+
+    def verifica_se_o_jogo_terminou(self):
+        pass
+
+
+class Carta:
+    def __init__(self, id_carta, valor, elemento):
+        self.id = id_carta
+        self.valor = valor
+        self.elemento = elemento
