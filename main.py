a58d3f0209e40f2335fd26fbcc7327b1b341928d
Rafael <rafaeldutraribas@gmail.com>
2022-07-05 18:04:10 -0300

    main FEATURE mostra uma carta do diretorio img no meio da tela

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import classes_jogo
 
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
@@ -10,12 +11,22 @@ LARGURA = 500
 ALTURA = 480
 QUADROS_POR_SEGUNDO = 30
 
+POSICAO = (LARGURA//2, ALTURA//2)
+
 # inicializacao da janela pygame
 pygame.init()
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Jogo Legal")
 clock = pygame.time.Clock()
 
+# por todas as sptrites aqui
+mao_jogador = pygame.sprite.Group()
+
+# cria as cartas
+c = classes_jogo.Carta(1, 3, 'fogo', POSICAO)
+
+# adiciona cartas a mao
+mao_jogador.add(c)
 
 rodando = True
 while rodando:
@@ -27,9 +38,11 @@ while rodando:
             rodando = False
 
     # atualiza o estado do jogo
+    mao_jogador.update()
 
     # desenha
     tela.fill(PRETO)
+    mao_jogador.draw(tela)
 
     pygame.display.flip()
 
