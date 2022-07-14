93e9fd504d94318a77a127c758ae0538bcd6901a
Rafael <rafaeldutraribas@gmail.com>
2022-07-14 18:20:43 -0300

    classes_jogo.py > elementos_tela.py Movido classe Carta
    Classe Carta está no arquivo elementos_tela.py

--- classes_jogo.py
+++ classes_jogo.py
@@ -59,23 +59,6 @@ class Juiz:
             return False
 
 
-# heranca sendo utilizada
-class Carta(pygame.sprite.Sprite):
-    def __init__(self, id_carta, valor, elemento, posicao_carta):
-        altura_tela = 500
-        largura_offset_inicial = 225
-
-        pygame.sprite.Sprite.__init__(self)
-        self.clicked = False
-        self.id = id_carta
-        self.valor = valor
-        self.elemento = elemento
-        self.image = pygame.image.load(f'img/Card-Jitsu_Cards_full_{id_carta}.png').convert()
-        self.image = pygame.transform.scale(self.image, (100, 120))
-        self.rect = self.image.get_rect()
-        self.rect.center = (largura_offset_inicial + (posicao_carta * 125), (altura_tela * 3) // 4)
-
-
 class Deck:
     def __init__(self):
         self.cartas = []
