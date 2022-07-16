532096ac1011f842ee5ceb425784b2044502688a
Rafael <rafaeldutraribas@gmail.com>
2022-07-16 12:40:35 -0300

    dados_cartas.py Add obter_numero_de_cartas
    funcao que retona o numero de cartas no arquivo JSON

--- dados_cartas.py
+++ dados_cartas.py
@@ -14,3 +14,13 @@ def obter_valor_da_carta_e_elemento(id_carta):
     return valor_carta, elemento_carta
 
 
+def obter_numero_de_cartas():
+    f = open('cartas.json', 'r')
+
+    data = json.load(f)
+
+    numero_de_cartas = len(data)
+
+    f.close()
+
+    return numero_de_cartas
