25f98f175f114f61bfd80ef1ab2b9527c5a8f7cd
Rafael <rafaeldutraribas@gmail.com>
2022-07-17 20:15:11 -0300

    elementos_tela.py Mudou construtor Elementos
    o construtor das classes de cada elemento posiciona os elementos de maneira diferente dependendo se o elemento é do jogador ou do computador

--- elementos_tela.py
+++ elementos_tela.py
@@ -18,32 +18,42 @@ class Carta(pygame.sprite.Sprite):
 
 
 class Fogo(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, jogador):
 
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load(f'img/fogo.png').convert()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.center = 200,100
 
+        if jogador:
+            self.rect.center = 200,100
+        else:
+            self.rect.center = 600,100
 
 class Agua(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, jogador):
 
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load(f'img/agua.png').convert()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.center = 100,100
+        if jogador:
+            self.rect.center = 100,100
+        else:
+            self.rect.center = 700,100
+
 
 class Gelo(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, jogador):
 
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load(f'img/gelo.png').convert()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.center = 300,100
+        if jogador:
+            self.rect.center = 300,100
+        else:
+            self.rect.center = 500,100
