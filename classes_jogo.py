fe99bed7f2d1896e4af37b845b76ccb29f570fb1
rafa <rafaeldutraribas@gmail.com>
2024-02-05 08:02:33 -0300

    WIP: load cards data on Deck

--- classes_jogo.py
+++ classes_jogo.py
@@ -1,5 +1,6 @@
 import random
-
+from typing import Any
+import json
 
 class Juiz:
     def __init__(self):
@@ -27,6 +28,27 @@ class Juiz:
             return jogador_ganhou
         else:
             return bot_ganhou
+
+    def stroger_card(self, card_0, card_1):
+        result = self.stronger_element(card_0, card_1)
+
+        if result is None:
+            return self.stroger_value(card_0, card_1)
+        else:
+            return result
+
+    def stronger_element(self, card_0, card_1):
+        if card_0.element == card_1.element: 
+            return None
+
+    def stronger_value(self, card_0, card_1):
+        if card_0.value == card_1.value:
+            return None
+        
+        if card_0.value > card_1.value:
+            return card_0
+        else:
+            return card_1
 
     def qual_elemento_ganha_retorna_none_caso_empate(self, elemento_0, elemento_1):
         index_elemento_0 = self.elementos_cartas.index(elemento_0)
@@ -68,8 +90,16 @@ class Juiz:
 class Deck:
     def __init__(self, tamnho_deck):
         self.cartas = []
+        self.cards_attrs = self.load_cards_attrs()
+        
         for i in range(1, tamnho_deck+1):
             self.cartas.append(i)
+
+    def load_cards_attrs(self):
+        f = open('cards.json', 'r')
+        cards_attrs = json.load(f)
+        f.close()
+        return cards_attrs
 
     def comprar_carta(self):
         if self.cartas:
@@ -78,3 +108,9 @@ class Deck:
         else:
             return -1
 
+class Card:
+    def __init__(self, id, owner, card_attr) -> None:
+        self.id = id
+        self.owner = owner
+        self.value = card_attr['value']
+        self.element = card_attr['element']
