ab570568fb7fe24d16cf216eadd846815cd513ac
rafa <rafaeldutraribas@gmail.com>
2024-02-06 11:38:51 -0300

    remove: unused function

--- dados_cartas.py
+++ dados_cartas.py
@@ -1,18 +1,4 @@
 import json
-import pygame
-
-def obter_valor_da_carta_e_elemento(id_carta):
-    f = open('cards.json', 'r')
-
-    data = json.load(f)
-
-    valor_carta = data[id_carta - 1]['value']
-    elemento_carta = data[id_carta - 1]['element']
-
-    f.close()
-
-    return valor_carta, elemento_carta
-
 
 def obter_numero_de_cartas():
     f = open('cards.json', 'r')
@@ -24,9 +10,3 @@ def obter_numero_de_cartas():
     f.close()
 
     return numero_de_cartas
-
-
-def imagem_carta(id_carta):
-    image = pygame.image.load(f'img/Card-Jitsu_Cards_full_{id_carta}.png').convert()
-
-    return pygame.transform.scale(image, (100, 120))
